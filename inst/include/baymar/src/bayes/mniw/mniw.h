2890d66c188510d00c2fa8d8fa128f2b5a5bb188
ygeunkim <ygeunkimstat@gmail.com>
2025-04-28 20:19:30 +0900

    add mniw header

---
+++ inst/include/baymar/src/bayes/mniw/mniw.h
@@ -0,0 +1,6 @@
+#ifndef BAYMAR_BAYES_MNIW_MNIW_H
+#define BAYMAR_BAYES_MNIW_MNIW_H
+
+#include "../misc/coef_helper.h"
+
+#endif // BAYMAR_BAYES_MNIW_MNIW_H
---
+++ src/mniw.cpp
@@ -0,0 +1,2 @@
+#include <baymar/mniw>
+
