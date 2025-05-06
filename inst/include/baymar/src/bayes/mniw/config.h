5f4fb540f6965c3cd56abe282852a00899ebae03
ygeunkim <ygeunkimstat.gmail.com>
2025-05-06 15:59:41 +0900

    use base mcmc class of bvhar after updating it

--- inst/include/baymar/src/bayes/mniw/config.h
+++ inst/include/baymar/src/bayes/mniw/config.h
@@ -9,18 +9,19 @@ namespace baymar {
 struct MatMniwParams;
 struct MatMniwInits;
 
-struct MatMniwParams {
+struct MatMniwParams : public bvhar::McmcParams {
 	std::vector<Eigen::SparseMatrix<double>> _x;
 	std::vector<Eigen::MatrixXd> _y;
-	int _iter, _row, _col, _design;
+	int _row, _col, _design;
 	Eigen::MatrixXd _row_mean, _row_iw_scl;
 	Eigen::MatrixXd _col_mean, _col_iw_scl;
 	Eigen::VectorXd _row_prec, _col_prec;
 	double _row_iw_df, _col_iw_df;
 
 	MatMniwParams(int num_iter, std::vector<Eigen::SparseMatrix<double>>& x, std::vector<Eigen::MatrixXd>& y, LIST& priors)
-	: _x(x), _y(y),
-		_iter(num_iter), _row(y[0].rows()), _col(y[0].cols()), _design(y.size()),
+	: bvhar::McmcParams(num_iter),
+		_x(x), _y(y),
+		_row(y[0].rows()), _col(y[0].cols()), _design(y.size()),
 		_row_mean(CAST<Eigen::MatrixXd>(priors["row_prior_mean"])), _row_iw_scl(CAST<Eigen::MatrixXd>(priors["row_iw_scl"])),
 		_col_mean(CAST<Eigen::MatrixXd>(priors["col_prior_mean"])), _col_iw_scl(CAST<Eigen::MatrixXd>(priors["col_iw_scl"])),
 		_row_prec(CAST<Eigen::MatrixXd>(priors["row_prior_prec"]).diagonal()), _col_prec(CAST<Eigen::MatrixXd>(priors["col_prior_prec"]).diagonal()),
