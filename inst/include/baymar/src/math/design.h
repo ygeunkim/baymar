ebadbf21b6ac9dc920fbb7a4e9469e05837c74ea
ygeunkim <ygeunkimstat.gmail.com>
2025-05-12 14:56:09 +0900

    add forecaster class derived from bvhar

--- inst/include/baymar/src/math/design.h
+++ inst/include/baymar/src/math/design.h
@@ -5,6 +5,39 @@
 
 namespace baymar {
 
+inline Eigen::SparseMatrix<double> build_blk_design(const std::vector<Eigen::MatrixXd>& y, int lag) {
+	int num_row = y[0].rows();
+	int num_col = y[0].cols();
+	Eigen::SparseMatrix<double> x(num_row * lag, num_col * lag);
+	Eigen::MatrixXd dense_x = x.toDense();
+	for (int i = 0; i < lag; ++i) {
+		dense_x.block(i * num_row, i * num_col, num_row, num_col) = y[i];
+	}
+	return dense_x.sparseView();
+}
+
+// y is (Y_1^T, ..., Y_T^T)^T
+inline Eigen::MatrixXd build_dense_design(const Eigen::MatrixXd& y, int lag) {
+	int num_row = y.rows() / lag;
+	int num_col = y.cols();
+	Eigen::MatrixXd dense_x(num_row * lag, num_col * lag);
+	for (int i = 0; i < lag; ++i) {
+		dense_x.block(i * num_row, i * num_col, num_row, num_col) = y.middleRows(i * num_row, num_row);
+	}
+	return dense_x;
+}
+
+inline void fill_lower(Eigen::Ref<Eigen::MatrixXd> lower_matrix, Eigen::Ref<const Eigen::VectorXd> lower_vec) {
+	int dim = lower_matrix.cols();
+  int id = 0;
+	int len = 0;
+	for (int i = 0; i < dim; ++i) {
+		len = dim - i;
+		lower_matrix.col(i).segment(i, len) = lower_vec.segment(id, len);
+		id += len;
+	}
+}
+
 // Get Y_{p + 1} = A^T X B
 // @param y Y_{p + 1}
 // @param x Initial diag(Y_1, ..., Y_p)
