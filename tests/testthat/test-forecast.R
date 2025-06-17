f99f34d4ec46e66947c65b94bfc2f550e48f81ab
ygeunkim <ygeunkimstat@gmail.com>
2025-06-17 11:19:11 +0900

    forecast_expand does not work

--- tests/testthat/test-forecast.R
+++ tests/testthat/test-forecast.R
@@ -109,39 +109,58 @@ test_that("Hierarchical Minnesota Prior - Rolling", {
   )
 })
 
-help_bmar_expand <- function(row_spec, col_spec) {
+help_bmar_expand <- function(row_spec, col_spec, exogen_row_spec = NULL, exogen_col_spec = NULL) {
   toy_data <- chanqi2025[1:2, 1:3, 1:10]
   eval_data <- chanqi2025[1:2, 1:3, 11:12]
+  exogen <- NULL
+  newxreg <- NULL
+  if (!(is.null(exogen_row_spec) && is.null(exogen_col_spec))) {
+    exogen <- chanqi2025[3:4, 4:5, 1:10]
+    newxreg <- chanqi2025[3:4, 4:5, 11:12]
+  }
   set.seed(1)
   fit_test <- mar_bayes(
     toy_data,
     p = 1,
+    exogen = exogen,
+    s = 0,
     num_chains = 2,
     num_iter = 5,
     num_burn = 2,
     thinning = 1,
     row_spec = row_spec,
     col_spec = col_spec,
+    exogen_row_spec = exogen_row_spec,
+    exogen_col_spec = exogen_col_spec,
     num_thread = 1
   )
   set.seed(1)
-  forecast_expand(fit_test, 1, eval_data)
+  forecast_expand(fit_test, 1, y_test = eval_data, newxreg = newxreg)
 }
 
 test_that("Minnesota Prior - Expanding", {
   expect_no_error(
     pred_test <- help_bmar_expand(set_mar_minnesota(kappa = .1), set_mar_minnesota(kappa = .1))
   )
+  # expect_no_error(
+  #   pred_test <- help_bmar_expand(set_mar_minnesota(kappa = .1), set_mar_minnesota(kappa = .1), set_mar_minnesota(kappa = .1), set_mar_minnesota(kappa = .1))
+  # )
 })
 
 test_that("Horseshoe Prior - Expanding", {
   expect_no_error(
     pred_test <- help_bmar_expand(set_mar_horseshoe(), set_mar_horseshoe())
   )
+  # expect_no_error(
+  #   pred_test <- help_bmar_expand(set_mar_horseshoe(), set_mar_horseshoe(), set_mar_horseshoe(), set_mar_horseshoe())
+  # )
 })
 
 test_that("Hierarchical Minnesota Prior - Expanding", {
   expect_no_error(
     pred_test <- help_bmar_expand(set_mar_minnesota(), set_mar_minnesota())
   )
+  # expect_no_error(
+  #   pred_test <- help_bmar_expand(set_mar_minnesota(), set_mar_minnesota(), set_mar_minnesota(), set_mar_minnesota())
+  # )
 })
