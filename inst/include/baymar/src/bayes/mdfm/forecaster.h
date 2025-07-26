15a2c21a9ebabb5e9dc5ac55458d8b6a77821504
ygeunkim <ygeunkimstat@gmail.com>
2025-07-26 06:29:16 -0700

    add out of sample forecaster for mdfm

--- inst/include/baymar/src/bayes/mdfm/forecaster.h
+++ inst/include/baymar/src/bayes/mdfm/forecaster.h
@@ -10,9 +10,9 @@ namespace baymar {
 class MatDfmForecaster;
 class MatDfmVarForecaster;
 class MatDfmForecastRun;
-template <bool> class MatMDfmOutForecastRun;
-template <bool> class MatMDfmRollForecastRun;
-template <bool> class MatMDfmExpandForecastRun;
+template <bool> class MatDfmOutForecastRun;
+template <bool> class MatDfmRollForecastRun;
+template <bool> class MatDfmExpandForecastRun;
 
 class MatDfmForecaster : public bvhar::BayesForecaster<Eigen::MatrixXd, Eigen::MatrixXd> {
 public:
@@ -189,6 +189,263 @@ public:
 	}
 	virtual ~MatDfmForecastRun() = default;
 };
+
+template <bool isUpdate = true>
+class MatDfmOutForecastRun : public bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate> {
+public:
+	MatDfmOutForecastRun(
+		const Eigen::MatrixXd& y, int num_data,
+		int num_chains, int num_iter, int num_burn, int thin, BVHAR_LIST& fit_record,
+		BVHAR_LIST& param_coef_sig, BVHAR_LIST_OF_LIST& coef_sig_init,
+		BVHAR_LIST& row_prior, BVHAR_LIST_OF_LIST& row_init, const int row_prior_type,
+		BVHAR_LIST& col_prior, BVHAR_LIST_OF_LIST& col_init, const int col_prior_type,
+		int nrow_factor, int ncol_factor, int factor_lag,
+		int step, const Eigen::MatrixXd& y_test,
+		const Eigen::MatrixXi& seed_chain, const Eigen::VectorXi& seed_forecast, bool display_progress, int nthreads
+	)
+	: bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>(
+			num_data, 1, num_chains, num_iter, num_burn, thin, step, y_test, false,
+			seed_chain, seed_forecast, display_progress, nthreads
+		),
+		num_row(y.rows() / num_data), num_col(y.cols()),
+		nrow_factor(nrow_factor), ncol_factor(ncol_factor), factor_lag(factor_lag) {
+		BVHAR_DEBUG_LOG(debug_logger, "MatDfmOutForecastRun Constructor: num_data={}, row_prior_type={}, col_prior_type={}", num_data, row_prior_type, col_prior_type);
+		num_test /= num_row;
+		num_horizon = num_test - step + 1;
+		roll_mat.resize(num_horizon);
+		model.resize(num_horizon);
+		out_forecast.resize(num_horizon);
+		lpl_record.resize(num_horizon, num_chains);
+		lpl_record = Eigen::MatrixXd::Zero(num_horizon, num_chains);
+	}
+	virtual ~MatDfmOutForecastRun() = default;
+
+protected:
+	int num_row, num_col, nrow_factor, ncol_factor, factor_lag;
+	using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::num_window;
+	using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::num_test;
+	using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::num_horizon;
+	using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::step;
+	using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::lag;
+	using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::num_chains;
+	using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::num_iter;
+	using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::num_burn;
+	using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::thin;
+	using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::nthreads;
+	// using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::get_lpl;
+	using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::display_progress;
+	using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::seed_forecast;
+	using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::roll_mat;
+	// using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::roll_y0;
+	using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::y_test;
+	using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::model;
+	using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::forecaster;
+	using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::out_forecast;
+	using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::lpl_record;
+	using bvhar::McmcOutForecastRun<Eigen::MatrixXd, Eigen::MatrixXd, isUpdate>::debug_logger;
+
+	Eigen::MatrixXd getValid() override {
+		BVHAR_DEBUG_LOG(debug_logger, "getValid() called");
+		return y_test.bottomRows(num_row);
+	}
+
+	void initialize(
+		const Eigen::MatrixXd& y, BVHAR_LIST& fit_record,
+		BVHAR_LIST& param_coef_sig, BVHAR_LIST_OF_LIST& coef_sig_init,
+		BVHAR_LIST& row_prior, BVHAR_LIST_OF_LIST& row_init, const int row_prior_type,
+		BVHAR_LIST& col_prior, BVHAR_LIST_OF_LIST& col_init, const int col_prior_type,
+		int nrow_factor, int ncol_factor, int factor_lag,
+		const Eigen::MatrixXi& seed_chain
+	) {
+		BVHAR_DEBUG_LOG(debug_logger, "initialize(...) called");
+		initData(y);
+		using is_mcmc = std::integral_constant<bool, isUpdate>;
+		if (is_mcmc::value) {
+			for (int window = 0; window < num_horizon; ++window) {
+				std::vector<Eigen::MatrixXd> y_data = marmatrix_to_vector(roll_mat[window], num_row);
+				std::vector<Eigen::MatrixXd> response = build_mar_response(y_data, lag);
+				auto temp_mcmc = initialize_matdfm(
+					num_chains, num_iter - num_burn, response, factor_lag,
+					param_coef_sig, coef_sig_init,
+					row_prior, row_init, row_prior_type,
+					col_prior, col_init, col_prior_type,
+					seed_chain.row(window)
+				);
+				auto temp_forecaster = initialize_matdfmforecaster(
+					num_chains, step, nrow_factor, ncol_factor, factor_lag,
+					fit_record, seed_forecast, nthreads
+				);
+				for (int i = 0; i < num_chains; ++i) {
+					model[window][i] = std::move(temp_mcmc[i]);
+					forecaster[window][i] = std::move(temp_forecaster[i]);
+				}
+			}
+		} else {
+			auto temp_forecaster = initialize_matdfmforecaster(
+				num_chains, step, nrow_factor, ncol_factor, factor_lag,
+				fit_record, seed_forecast, nthreads
+			);
+			for (int i = 0; i < num_chains; ++i) {
+				forecaster[0][i] = std::move(temp_forecaster[i]);
+			}
+		}
+	}
+
+	virtual void initData(const Eigen::MatrixXd& y) = 0;
+
+	void updateForecaster(int window, int chain) override {
+		BVHAR_DEBUG_LOG(debug_logger, "updateForecaster(window={}, chain={}) called", window, chain);
+		auto* mcmc_mdfm = dynamic_cast<McmcMatDfm*>(model[window][chain].get());
+		MatDfmVarRecords mdfm_var_record = mcmc_mdfm->returnStructRecords<MatDfmVarRecords>(0, thin);
+		auto factor_updater = std::make_unique<MatFactorVarForecaster>(mdfm_var_record, step, factor_lag, num_row, num_col, nrow_factor, ncol_factor);
+		forecaster[window][chain] = std::make_unique<MatDfmVarForecaster>(mdfm_var_record, factor_updater, step, static_cast<unsigned int>(seed_forecast[chain]));
+	}
+};
+
+template <bool isUpdate = true>
+class MatDfmRollForecastRun : public MatDfmOutForecastRun<isUpdate> {
+public:
+	MatDfmRollForecastRun(
+		const Eigen::MatrixXd& y, int num_data,
+		int num_chains, int num_iter, int num_burn, int thin, BVHAR_LIST& fit_record,
+		BVHAR_LIST& param_coef_sig, BVHAR_LIST_OF_LIST& coef_sig_init,
+		BVHAR_LIST& row_prior, BVHAR_LIST_OF_LIST& row_init, const int row_prior_type,
+		BVHAR_LIST& col_prior, BVHAR_LIST_OF_LIST& col_init, const int col_prior_type,
+		int nrow_factor, int ncol_factor, int factor_lag,
+		int step, const Eigen::MatrixXd& y_test,
+		const Eigen::MatrixXi& seed_chain, const Eigen::VectorXi& seed_forecast, bool display_progress, int nthreads
+	)
+	: MatDfmOutForecastRun<isUpdate>(
+			y, num_data, num_chains, num_iter, num_burn, thin, fit_record,
+			param_coef_sig, coef_sig_init, row_prior, row_init, row_prior_type, col_prior, col_init, col_prior_type,
+			nrow_factor, ncol_factor, factor_lag,
+			step, y_test,
+			seed_chain, seed_forecast, display_progress, nthreads
+		) {
+		BVHAR_DEBUG_LOG(debug_logger, "MatDfmOutForecastRun constructor");
+		initialize(
+			y, fit_record,
+			param_coef_sig, coef_sig_init,
+			row_prior, row_init, row_prior_type, col_prior, col_init, col_prior_type,
+			nrow_factor, ncol_factor, factor_lag, seed_chain
+		);
+	}
+	virtual ~MatDfmRollForecastRun() = default;
+
+protected:
+	using MatDfmOutForecastRun<isUpdate>::num_window;
+	using MatDfmOutForecastRun<isUpdate>::num_row;
+	using MatDfmOutForecastRun<isUpdate>::num_col;
+	using MatDfmOutForecastRun<isUpdate>::num_test;
+	using MatDfmOutForecastRun<isUpdate>::num_horizon;
+	using MatDfmOutForecastRun<isUpdate>::step;
+	using MatDfmOutForecastRun<isUpdate>::roll_mat;
+	using MatDfmOutForecastRun<isUpdate>::y_test;
+	using MatDfmOutForecastRun<isUpdate>::initialize;
+	using MatDfmOutForecastRun<isUpdate>::roll_exogen_mat;
+	using MatDfmOutForecastRun<isUpdate>::roll_exogen;
+	using MatDfmOutForecastRun<isUpdate>::lag_exogen;
+	using MatDfmOutForecastRun<isUpdate>::debug_logger;
+
+	void initData(const Eigen::MatrixXd& y) override {
+		BVHAR_DEBUG_LOG(debug_logger, "initData(y, ...) called");
+		Eigen::MatrixXd tot_mat((num_window + num_test) * num_row, num_col);
+		tot_mat << y,
+							 y_test;
+		for (int i = 0; i < num_horizon; ++i) {
+			// roll_mat[i] = tot_mat.middleRows(i * num_row, num_window);
+			roll_mat[i] = tot_mat.middleRows(i * num_row, num_window * num_row);
+			// roll_y0[i] = roll_mat[i].bottomRows(num_window - num_row * lag);
+		}
+	}
+};
+
+template <bool isUpdate = true>
+class MatDfmExpandForecastRun : public MatDfmOutForecastRun<isUpdate> {
+public:
+	MatDfmExpandForecastRun(
+		const Eigen::MatrixXd& y, int num_data,
+		int num_chains, int num_iter, int num_burn, int thin, BVHAR_LIST& fit_record,
+		BVHAR_LIST& param_coef_sig, BVHAR_LIST_OF_LIST& coef_sig_init,
+		BVHAR_LIST& row_prior, BVHAR_LIST_OF_LIST& row_init, const int row_prior_type,
+		BVHAR_LIST& col_prior, BVHAR_LIST_OF_LIST& col_init, const int col_prior_type,
+		int nrow_factor, int ncol_factor, int factor_lag,
+		int step, const Eigen::MatrixXd& y_test,
+		const Eigen::MatrixXi& seed_chain, const Eigen::VectorXi& seed_forecast, bool display_progress, int nthreads
+	)
+	: MatDfmOutForecastRun<isUpdate>(
+			y, num_data, num_chains, num_iter, num_burn, thin, fit_record,
+			param_coef_sig, coef_sig_init, row_prior, row_init, row_prior_type, col_prior, col_init, col_prior_type,
+			nrow_factor, ncol_factor, factor_lag,
+			step, y_test,
+			seed_chain, seed_forecast, display_progress, nthreads
+		) {
+		BVHAR_DEBUG_LOG(debug_logger, "MatDfmOutForecastRun constructor");
+		initialize(
+			y, fit_record,
+			param_coef_sig, coef_sig_init,
+			row_prior, row_init, row_prior_type, col_prior, col_init, col_prior_type,
+			nrow_factor, ncol_factor, factor_lag, seed_chain
+		);
+	}
+	virtual ~MatDfmExpandForecastRun() = default;
+
+protected:
+	using MatDfmOutForecastRun<isUpdate>::num_window;
+	using MatDfmOutForecastRun<isUpdate>::num_row;
+	using MatDfmOutForecastRun<isUpdate>::num_col;
+	using MatDfmOutForecastRun<isUpdate>::num_test;
+	using MatDfmOutForecastRun<isUpdate>::num_horizon;
+	using MatDfmOutForecastRun<isUpdate>::step;
+	using MatDfmOutForecastRun<isUpdate>::roll_mat;
+	using MatDfmOutForecastRun<isUpdate>::y_test;
+	using MatDfmOutForecastRun<isUpdate>::initialize;
+	using MatDfmOutForecastRun<isUpdate>::roll_exogen_mat;
+	using MatDfmOutForecastRun<isUpdate>::roll_exogen;
+	using MatDfmOutForecastRun<isUpdate>::lag_exogen;
+	using MatDfmOutForecastRun<isUpdate>::debug_logger;
+
+	void initData(const Eigen::MatrixXd& y) override {
+		BVHAR_DEBUG_LOG(debug_logger, "initData(y, ...) called");
+		Eigen::MatrixXd tot_mat((num_window + num_test) * num_row, num_col);
+		tot_mat << y,
+							 y_test;
+		for (int i = 0; i < num_horizon; ++i) {
+			roll_mat[i] = tot_mat.topRows((num_window + i) * num_row);
+			BVHAR_DEBUG_LOG(debug_logger, "roll_mat[{}]: {} x {}", i, roll_mat[i].rows(), roll_mat[i].cols());
+		}
+	}
+};
+
+template <template <bool> class BaseOutForecast = MatDfmRollForecastRun>
+inline std::unique_ptr<bvhar::McmcOutforecastInterface> initialize_matdfmoutforecaster(
+	const Eigen::MatrixXd& y, int num_data,
+	int num_chains, int num_iter, int num_burn, int thin, BVHAR_LIST& fit_record,
+	bool run_mcmc,
+	BVHAR_LIST& param_coef_sig, BVHAR_LIST_OF_LIST& coef_sig_init,
+	BVHAR_LIST& row_prior, BVHAR_LIST_OF_LIST& row_init, const int row_prior_type,
+	BVHAR_LIST& col_prior, BVHAR_LIST_OF_LIST& col_init, const int col_prior_type,
+	int nrow_factor, int ncol_factor, int factor_lag,
+	int step, const Eigen::MatrixXd& y_test,
+	const Eigen::MatrixXi& seed_chain, const Eigen::VectorXi& seed_forecast, bool display_progress, int nthreads
+) {
+	if (run_mcmc) {
+		return std::make_unique<BaseOutForecast<true>>(
+			y, num_data, num_chains, num_iter, num_burn, thin, fit_record,
+			param_coef_sig, coef_sig_init, row_prior, row_init, row_prior_type, col_prior, col_init, col_prior_type,
+			nrow_factor, ncol_factor, factor_lag,
+			step, y_test,
+			seed_chain, seed_forecast, display_progress, nthreads
+		);
+	}
+	return std::make_unique<BaseOutForecast<false>>(
+		y, num_data, num_chains, num_iter, num_burn, thin, fit_record,
+		param_coef_sig, coef_sig_init, row_prior, row_init, row_prior_type, col_prior, col_init, col_prior_type,
+		nrow_factor, ncol_factor, factor_lag,
+		step, y_test,
+		seed_chain, seed_forecast, display_progress, nthreads
+	);
+}
 
 } // namespace baymar
 
