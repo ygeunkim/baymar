84543a2591a58f5f1ce840cba6d9f590be1ff3f0
ygeunkim <ygeunkimstat@gmail.com>
2025-08-02 10:26:56 +0900

    horseshoe didn't update precision

--- inst/include/baymar/src/bayes/shrinkage/shrinkage.h
+++ inst/include/baymar/src/bayes/shrinkage/shrinkage.h
@@ -67,7 +67,7 @@ public:
 	virtual ~MatHsUpdater() = default;
 	
 	void initPrec(Eigen::Ref<Eigen::VectorXd> prior_prec) override {
-		prior_prec.array() /= (global_lev * local_lev.array());
+		prior_prec.array() /= (global_lev * local_lev.array()).square();
 	}
 
 	void updatePrec(
@@ -79,6 +79,7 @@ public:
 		bvhar::horseshoe_latent(latent_local, local_lev, rng);
 		bvhar::horseshoe_latent(latent_global, global_lev, rng);
 		horseshoe_sparsity(local_lev, global_lev, prior_prec, coef, sig_lower, latent_local, latent_global, rng);
+		prior_prec = 1 / (global_lev * local_lev.array()).square();
 	}
 
 private:
