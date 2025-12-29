90f8b215162eaeb734798a4335f18731cab93881
ygeunkim <ygeunkimstat@gmail.com>
2025-12-29 16:51:41 +0900

    remove sigma_c restricted draw [skip ci]

--- inst/include/baymar/src/bayes/misc/coef_helper.h
+++ inst/include/baymar/src/bayes/misc/coef_helper.h
@@ -180,13 +180,14 @@ inline void draw_coef_sig(
 	Eigen::MatrixXd post_mean = llt_of_prec.solve(post_solve);
 	post_iw_scl -= post_mean.transpose() * post_cov * post_mean;
 	double post_df = iw_df + num_mat * other_dim;
-	if (is_row::value) {
-		sig_lower = bvhar::sim_iw_tri(post_iw_scl, post_df, rng);
-	} else {
-		// Indentifiability restriction
-		// Sigma_c(1, 1) = 1
-		sig_lower = sim_iw_tri_restr(post_iw_scl, post_df, rng);
-	}
+	sig_lower = bvhar::sim_iw_tri(post_iw_scl, post_df, rng);
+	// if (is_row::value) {
+	// 	sig_lower = bvhar::sim_iw_tri(post_iw_scl, post_df, rng);
+	// } else {
+	// 	// Indentifiability restriction
+	// 	// Sigma_c(1, 1) = 1
+	// 	sig_lower = sim_iw_tri_restr(post_iw_scl, post_df, rng);
+	// }
 	for (int i = 0; i < prior_mean.rows(); ++i) {
 		for (int j = 0; j < prior_mean.cols(); ++j) {
 			coef(i, j) = bvhar::normal_rand(rng); // MN(0, I_n, I_k)
