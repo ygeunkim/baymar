3f63ee77aff687c74186cfb9c5884be51f2123be
ygeunkim <ygeunkimstat@gmail.com>
2026-01-08 16:34:07 +0900

    Revert "use macro for different method to draw factors"
    This reverts commit f4eb2156379f2c19875a87acebedecdfcc085951.

--- inst/include/baymar/src/bayes/mniw/mniw.h
+++ inst/include/baymar/src/bayes/mniw/mniw.h
@@ -237,36 +237,33 @@ protected:
 		// );
 		bool factor_restrict = false;
 		if (famar_updater) {
-			factor_restrict = famar_updater->NeedsRestrict();
-	#ifdef BAYMAR_FIT_RESID
-			famar_updater->updateResponse(
-				y,
-				row_coef.bottomRows(nrow_factor),
-				col_coef.bottomRows(ncol_factor)
-			);
-			draw_coef_sig<true>(
-				row_coef.topRows(nrow_row_coef + nrow_row_exogen), row_sig_lower,
-				col_coef.topRows(nrow_col_coef + nrow_col_exogen), col_sig_lower,
-				row_prior_mean.topRows(nrow_row_coef + nrow_row_exogen),
-				row_prior_prec.head(nrow_row_coef + nrow_row_exogen),
-				row_iw_scl, row_iw_df,
-				num_design, num_col,
-				nrow_row_exogen, exogen_lag,
-				0, false,
-				x, famar_updater->getResid(), rng
-			);
-			draw_coef_sig<false>(
-				col_coef.topRows(nrow_col_coef + nrow_col_exogen), col_sig_lower,
-				row_coef.topRows(nrow_row_coef + nrow_row_exogen), row_sig_lower,
-				col_prior_mean.topRows(nrow_col_coef + nrow_col_exogen),
-				col_prior_prec.head(nrow_col_coef + nrow_col_exogen),
-				col_iw_scl, col_iw_df,
-				num_design, num_row,
-				nrow_col_exogen, exogen_lag,
-				0, false,
-				x, famar_updater->getResid(), rng
-			);
-	#endif
+			// famar_updater->updateResponse(
+			// 	y,
+			// 	row_coef.bottomRows(nrow_factor),
+			// 	col_coef.bottomRows(ncol_factor)
+			// );
+			// draw_coef_sig<true>(
+			// 	row_coef.topRows(nrow_row_coef + nrow_row_exogen), row_sig_lower,
+			// 	col_coef.topRows(nrow_col_coef + nrow_col_exogen), col_sig_lower,
+			// 	row_prior_mean.topRows(nrow_row_coef + nrow_row_exogen),
+			// 	row_prior_prec.head(nrow_row_coef + nrow_row_exogen),
+			// 	row_iw_scl, row_iw_df,
+			// 	num_design, num_col,
+			// 	nrow_row_exogen, exogen_lag,
+			// 	0, false,
+			// 	x, famar_updater->getResid(), rng
+			// );
+			// draw_coef_sig<false>(
+			// 	col_coef.topRows(nrow_col_coef + nrow_col_exogen), col_sig_lower,
+			// 	row_coef.topRows(nrow_row_coef + nrow_row_exogen), row_sig_lower,
+			// 	col_prior_mean.topRows(nrow_col_coef + nrow_col_exogen),
+			// 	col_prior_prec.head(nrow_col_coef + nrow_col_exogen),
+			// 	col_iw_scl, col_iw_df,
+			// 	num_design, num_row,
+			// 	nrow_col_exogen, exogen_lag,
+			// 	0, false,
+			// 	x, famar_updater->getResid(), rng
+			// );
 			famar_updater->updateResid(
 				x, y,
 				row_coef.topRows(nrow_row_coef + nrow_row_exogen), col_coef.topRows(nrow_col_coef + nrow_col_exogen)
@@ -276,47 +273,45 @@ protected:
 				col_coef.bottomRows(ncol_factor), col_sig_lower,
 				rng
 			);
-	#if !defined(BAYMAR_FIT_RESID)
 			famar_updater->appendDesign(x);
-	#endif
-	#ifdef BAYMAR_FIT_RESID
-			famar_updater->updateCoefCov<true>(
-				row_coef.bottomRows(nrow_factor), row_sig_lower,
-				col_coef.bottomRows(ncol_factor), col_sig_lower,
-				row_prior_mean.bottomRows(nrow_factor),
-				row_prior_prec.tail(nrow_factor),
-				row_iw_scl, row_iw_df,
-				num_col, rng
-			);
-			famar_updater->updateCoefCov<false>(
-				col_coef.bottomRows(ncol_factor), col_sig_lower,
-				row_coef.bottomRows(nrow_factor), row_sig_lower,
-				col_prior_mean.bottomRows(ncol_factor),
-				col_prior_prec.tail(ncol_factor),
-				col_iw_scl, col_iw_df,
-				num_row, rng
-			);
-		} else {
-			draw_coef_sig<true>(
-				row_coef, row_sig_lower,
-				col_coef, col_sig_lower,
-				row_prior_mean, row_prior_prec, row_iw_scl, row_iw_df,
-				num_design, num_col,
-				nrow_row_exogen, exogen_lag,
-				nrow_factor, factor_restrict,
-				x, y, rng
-			);
-			draw_coef_sig<false>(
-				col_coef, col_sig_lower,
-				row_coef, row_sig_lower,
-				col_prior_mean, col_prior_prec, col_iw_scl, col_iw_df,
-				num_design, num_row,
-				nrow_col_exogen, exogen_lag,
-				ncol_factor, factor_restrict,
-				x, y, rng
-			);
-	#else
+			factor_restrict = famar_updater->NeedsRestrict();
+			// famar_updater->updateCoefCov<true>(
+			// 	row_coef.bottomRows(nrow_factor), row_sig_lower,
+			// 	col_coef.bottomRows(ncol_factor), col_sig_lower,
+			// 	row_prior_mean.bottomRows(nrow_factor),
+			// 	row_prior_prec.tail(nrow_factor),
+			// 	row_iw_scl, row_iw_df,
+			// 	num_col, rng
+			// );
+			// famar_updater->updateCoefCov<false>(
+			// 	col_coef.bottomRows(ncol_factor), col_sig_lower,
+			// 	row_coef.bottomRows(nrow_factor), row_sig_lower,
+			// 	col_prior_mean.bottomRows(ncol_factor),
+			// 	col_prior_prec.tail(ncol_factor),
+			// 	col_iw_scl, col_iw_df,
+			// 	num_row, rng
+			// );
 		}
+		// else {
+		// 	draw_coef_sig<true>(
+		// 		row_coef, row_sig_lower,
+		// 		col_coef, col_sig_lower,
+		// 		row_prior_mean, row_prior_prec, row_iw_scl, row_iw_df,
+		// 		num_design, num_col,
+		// 		nrow_row_exogen, exogen_lag,
+		// 		nrow_factor, factor_restrict,
+		// 		x, y, rng
+		// 	);
+		// 	draw_coef_sig<false>(
+		// 		col_coef, col_sig_lower,
+		// 		row_coef, row_sig_lower,
+		// 		col_prior_mean, col_prior_prec, col_iw_scl, col_iw_df,
+		// 		num_design, num_row,
+		// 		nrow_col_exogen, exogen_lag,
+		// 		ncol_factor, factor_restrict,
+		// 		x, y, rng
+		// 	);
+		// }
 		draw_coef_sig<true>(
 			row_coef, row_sig_lower,
 			col_coef, col_sig_lower,
@@ -335,7 +330,6 @@ protected:
 			ncol_factor, factor_restrict,
 			x, y, rng
 		);
-	#endif
 	}
 
 	void updateRecords() {
